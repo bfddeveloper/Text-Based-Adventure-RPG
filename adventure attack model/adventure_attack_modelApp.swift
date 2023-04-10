6aab58a293a0e5198ddef85153cc1a7831d643f9
bfddeveloper <dicksonbrody@icloud.com>
2023-04-10 15:17:19 -0500

    Initial Commit

---
+++ adventure attack model/adventure_attack_modelApp.swift
@@ -0,0 +1,17 @@
+//
+//  adventure_attack_modelApp.swift
+//  adventure attack model
+//
+//  Created by Brody Dickson on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct adventure_attack_modelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
