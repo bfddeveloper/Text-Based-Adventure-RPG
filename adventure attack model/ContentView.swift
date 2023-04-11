5753adc8960c1319549ee2675d527e5ed41c57c5
bfddeveloper <dicksonbrody@icloud.com>
2023-04-11 09:28:16 -0500

    first commit

--- adventure attack model/ContentView.swift
+++ adventure attack model/ContentView.swift
@@ -8,12 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var PlayerHealth = 25
+    @State var MonsterBlueHP = 10
+    @State var monsterescape = 0
+    @State var PlayerEscape = 0
+    @State var PlayerDamage = 2
+    @State var ReactionText = ""
+    @State var CanChange = true
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Your health: \(PlayerHealth)")
+            Text("a monster with bright blue teeth and a iguana shaped body but mich larger aproaches you what do you do?")
+            
+            Button("attack"){
+                if CanChange == true {
+                    ReactionText = "You attack the monster with your fists, the moster comes back ready to pounce at you"
+                    MonsterBlueHP -= PlayerDamage
+                    
+                }
+            }
+                Button("Run"){
+                    if CanChange == true {
+                        PlayerEscape = Int.random(in: 1..<7)
+                        monsterescape = Int.random(in: 1..<7)
+                        if PlayerEscape > monsterescape {
+                            ReactionText = "you succesfully ran away without a scratch"
+                            
+                            MonsterBlueHP = 0
+                            
+                            
+                        } else {
+                            ReactionText = "The monster caught as you were trying to sneak away you take 4 damage"
+                            
+                            PlayerHealth -= 4
+                        }
+                    }
+                }
+                
+        
+            Text(ReactionText)
+            HStack{
+                Text(String(PlayerHealth))
+                Text(String(MonsterBlueHP))
+            
+                
+            }
         }
         .padding()
     }
