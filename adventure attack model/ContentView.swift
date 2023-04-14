55652b1179ad1239233ad86a6b7637d221045d0e
bfddeveloper <dicksonbrody@icloud.com>
2023-04-14 09:33:11 -0500

    sweet up story buttons and stuff

--- adventure attack model/ContentView.swift
+++ adventure attack model/ContentView.swift
@@ -11,49 +11,81 @@
 import SwiftUI
 
 struct ContentView: View {
+    let story = Story()
     @State var MonsterBlueHP = 10
     @State var monsterescape = 0
     @State var PlayerEscape = 0
     @State var PlayerDamage = 2
     @State var ReactionText = ""
     @State var engagedMonster = ""
-    @State var storyText = 0
+    @State var storyText = ""
     @State var storytext = ""
     @State var CanChange = true
     @State var playerHealth = 25
+    @State var attackInitiation = 0.0
+    @State var storyIndex = 0
+    @State var lookOpac = 0.0
+    @State var continueOpac = 1.0
+    
+    @State var StoryArray = ["You are stranded inside a cave on the ground with nothing but a wand and some food","You Coninue forward through the cave ", "3"]
+    @State var LookDescArray = [ "There is walls made of old crumbling stone, infront of you, you can see a faint dot of light which appears to be the entrance to the cave. Behind you there is nothing a stone wall", " there is still just a cave light infront of you it looks closer"]
+    @State var descIndex = 0
+    
+    
     var body: some View {
         NavigationView{
             VStack {
                 Text("Your health: \(playerHealth)")
                 Text(storytext)
+                //
                 Button("Continue"){
-        
-                }
-                
-                NavigationLink("Attack", destination: AttackView(engagedMonster: engagedMonster, playerHealth: playerHealth))
-                    .onTapGesture {
-                        engagedMonster = "BlackRat"
+                    storytext = StoryArray[storyIndex]
+                    storyIndex += 1
+                    if storyIndex == 1 {
+                        lookOpac = 1.0
+                        continueOpac = 0.0
                     }
-                        
-                Button("Run"){
-                    if CanChange == true {
-                        PlayerEscape = Int.random(in: 1..<7)
-                        monsterescape = Int.random(in: 1..<7)
-                        if PlayerEscape > monsterescape {
-                            ReactionText = "you succesfully ran away without a scratch"
-                            
-                            MonsterBlueHP = 0
-                            
-                            
-                        } else {
-                            ReactionText = "The monster caught as you were trying to sneak away you take 4 damage"
-                            playerHealth -= 4
-                            
-
-                            
+                }
+                .opacity(continueOpac)
+                
+                Button("look around"){
+                    storytext = LookDescArray[descIndex]
+                    descIndex += 1
+                    continueOpac = 1.0
+                    lookOpac = 0.0
+                }
+                .opacity(lookOpac)
+                
+                //atck stuff
+                
+                HStack {
+                    NavigationLink("Attack", destination: AttackView(engagedMonster: engagedMonster, playerHealth: playerHealth))
+                        .onTapGesture {
+                            engagedMonster = "BlackRat"
+                        }
+                    
+                    Button("Run"){
+                        if CanChange == true {
+                            PlayerEscape = Int.random(in: 1..<7)
+                            monsterescape = Int.random(in: 1..<7)
+                            if PlayerEscape > monsterescape {
+                                ReactionText = "you succesfully ran away without a scratch"
+                                
+                                MonsterBlueHP = 0
+                                
+                                
+                            } else {
+                                ReactionText = "The monster caught as you were trying to sneak away you take 4 damage"
+                                playerHealth -= 4
+                                
+                                
+                                
+                            }
                         }
                     }
                 }
+                .opacity(attackInitiation)
+                //end
                 
                 
                 Text(ReactionText)
@@ -67,6 +99,8 @@ struct ContentView: View {
         }
         .padding()
     }
+    
+   
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -75,7 +109,13 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-s
+struct Story {
+    var Main1 = "You are stranded inside a cave on the ground with nothing but a wand and some food"
+    
+    var sceneOneDesc = "There is walls made of old crumbling stone, infront of you, you can see a faint dot of light which appears to be the entrance to the cave. Behind you there is nothing a stone wall"
+    
+}
+
 
 
 
